000c30a21bbd67976bd3a0ca29321cf8efe1894a
Chach Sikes <chachasikes@gmail.com>
2010-03-07 12:04:31 -0600

    initial import of markup

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta content="en-us" http-equiv="Content-Language" />
+<meta content="Neighborly connects you with your closest neighbors." name="description" />
+
+<title>Neighborly / Home</title>
+<link rel="stylesheet" href="theme/css/reset.css" />
+<link rel="stylesheet" href="theme/css/text.css" />
+<link rel="stylesheet" href="theme/css/960.css" />
+<?php include 'includes/neighborly.theme.inc'; ?>
+</head>
+<body>
+  <div id="page" class="container_20 page">
+    <div id="messages" class="grid_20 messages alpha omega">MESSAGES</div>
+    <div id="branding" class="grid_10 branding alpha">
+      <div id="logo" class="grid_2 logo alpha">LOGO</div>
+      <div id="title" class="grid_8 title">
+        <h1><a href="index.html">Neighborly</a></h1>
+      </div>
+    </div>
+    <div id="primary-nav" class="grid_10 primary-nav omega">
+      <ul class="menu">
+        <li><a href="learn">learn</a></li>
+        <li><a href="about">about</a></li>
+        <li><a href="signup">signup</a></li>
+        <li><a href="login">login</a></li>
+      </ul>
+  	</div>
+    <div id="content" class="grid_20 content alpha omega">
+      <div id="main-content" class="grid_10 main-content prefix_5">
+    	<h2 class="tagline">Project tagline in three to eight words.</h2>
+      <div id="register-address">
+        <form id="address">
+          <label>Enter your address</label>
+          <input value="" />
+          <div class="description">
+            to see your neighborly neighbors
+          </div>
+        </form>
+      </div>
+      </div>
+
+      <div id="sidebar" class="grid_5 sidebar-left alpha">
+        SIDEBAR LEFT
+      </div>
+
+      <div id="sidebar-right" class="grid_5 sidebar-right push_15 omega">
+        SIDEBAR RIGHT
+      </div>
+
+    </div>
+  	<div class="clear"></div>  
+    <div id="footer" class="grid_20 footer alpha omega">
+      <ul class="menu">
+        <li><a href="besafe">Be safe</a></li>
+        <li><a href="contact">Contact</a></li>
+        <li><a href="get-involved">Get involved</a></li>
+      </ul>
+    </div>
+  	<div class="clear"></div>
+    <div id="closure" class="grid_20 closure alpha omega">&copy; 2010 e-democracy.org</div>
+  	<div class="clear"></div>
+  </div>
+</body>
+</html>
